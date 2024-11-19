7e5305c089e4fa3bfd62b67473677322cde67229
Pratap Git <pratapkumarnayak09@gmail.com>
2024-11-19 12:08:11 +0530

    typescript tutorial

---
+++ pratice 1/variables.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var a = "Hello World";
+a.toUpperCase();
+console.log(a);
+// number
+var pi = 123.23;
+//boolean
+var isloggedin = false;
---
+++ pratice 1/variables.ts
@@ -0,0 +1,13 @@
+let a:string = "Hello World";
+a.toUpperCase();
+console.log(a);
+
+// number
+
+let pi:number = 123.23;
+
+//boolean
+
+let isloggedin:boolean = false;
+
+export{}
