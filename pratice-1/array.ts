071f1a3dbf3caa555fbf650eeaa67da628d98cb0
Pratap Git <pratapkumarnayak09@gmail.com>
2024-12-18 15:41:15 +0530

    array in ts

---
+++ pratice-1/array.ts
@@ -0,0 +1,14 @@
+let arr:number[] = [1,2,3,4,5];
+
+let arr2:string[] = new Array('a','b','c');
+
+//let arr3:string[] = Array.of("10","20","30")
+
+//console.log(arr3[1]);
+
+//console.log(arr.push()); // return length of the array
+//console.log(arr.pop());// return last deleted value
+console.log(arr.unshift(10));
+console.log(arr);
+
+
